423f1ff4b62bcf66952cf51c9573316f8c147ef2
khôi đình <dinhkhoi.le05@gmail.com>
2022-01-19 18:27:27 +0700

    _

--- src/requests/vietstock/corporate.ts
+++ src/requests/vietstock/corporate.ts
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import { loginCookies, retrieveCookiesAndCsrf } from './retrieveCookiesAndCsrf';
+import { VietStockCrds } from './credentials';
 
 const retrieveCor = async (sessionId: string, rvt: string, vtsUsrLg: string, usrTk: string, csrf: string) => {
   const r = await fetch('https://finance.vietstock.vn/data/corporateaz', {
@@ -30,26 +30,7 @@ const retrieveCor = async (sessionId: string, rvt: string, vtsUsrLg: string, usr
 
 export const getCorporate = async () => {
   try {
-    let sid = '1i1225usk5lljrcwny0tmffl';
-    let rvt = 'nLE53UKUb9eZX-5Nv3aMQ4jYuOZ-2Y9nvkGXZ0dAM1TYu_7tHQPsDhyrKF87cZu423xFKHggL0kq-ywWhRMEe8ZKpoH7Lc8X2QDQ0YSrfZM1';
-    let vtsUsrLg = '3C423246818F0E3528187CCAAC8884C7DC2AC16F9370B0F46310C7C97868240303DA0954B5ABFD2F42DBFAD6F7E849A820ED218753FBE20CFE166B2C0CF57CB3781FC08C58FD42AB62243B08DB47839FD9C85C2C1492899C1E2EAA852FB5384C5967E0EA4C74D4E0A3D959F9FED70743D68F7E7139D3C4B3B60DC6D726AE60D2';
-    let usrTk = 'wAoBVyA7RUuS5D/peNITBQ==';
-    let csrf = '84e0HL9eS6L8-r28ET34ruJFLjTAeN-KKoxiy2zU7VpPH7ujUsEdl99MyCYDOcVnvou37RSklZ7JBq8wf8f5iTbSOmRKF4TdsLtL7x0JFCEGkqUzMCRJ3Vq4c0qY6B180';
-
-    const afterLogin = await loginCookies();
-    const re = new RegExp('(.*)(ASP.NET_SessionId=.*;)(.*)(__RequestVerificationToken=.*;)(.*)(vts_usr_lg=.*;)(.*)(vst_usr_lg_token=.*;)(.*)');
-    const r = re.exec(afterLogin.cookies);
-    if (r.length === 10) {
-      const _getValue = (cValue: string) => {
-        return cValue.slice(cValue.indexOf('=') + 1, cValue.length - 1);
-      };
-      sid = _getValue(r[1]);
-      rvt = _getValue(r[4]);
-      vtsUsrLg = _getValue(r[6]);
-      usrTk = _getValue(r[8]);
-    }
-    const csrfAfterLogin = await retrieveCookiesAndCsrf(afterLogin.cookies, false);
-    csrf = csrfAfterLogin.csrf;
+    const { sid, rvt, vtsUsrLg, usrTk, csrf } = await VietStockCrds.retrieveCredentials();
     const t = await retrieveCor(sid, rvt, vtsUsrLg, usrTk, csrf);
     console.log(t);
   } catch (e) {
